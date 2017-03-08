08cc6de27e30978d1f37628d53350e57ff7ce2c4
thelokys <ricardo.guidetti@hotmail.com>
2017-03-08 09:19:41 -0300

    lab2

--- Laboratório de Programação II/[Lista01] Aquecimento!/Lista1/Principal.java
+++ Laboratório de Programação II/[Lista01] Aquecimento!/Lista1/Principal.java
@@ -1,133 +1,116 @@
-import java.util.Arrays;
-
 /**
- * Classe Principal.java
- *
- * @author Gabriel Praxedes, José Ricardo 
+ * Principal.java, classe com as respostas da "lista1.pdf"
+ * 
+ * @author Gabriel Praxedes, José Ricardo
+ * @version 07032017
  */
 public class Principal
 {
-
-    // 1 - DONE
-    public static long calc (int x) 
-    {
-        if(x < 2)
-        {
-            return x;
-        } else {
-            return calc(x-1) + calc(x-2);
-        }
-    }
-
-    // 2 - PRAXGOD DONE
-    //B
     /**
-     * Ordena o vetor passado no parâmetro e depois soma o segundo e o terceiro maior do vetor
+     * Calcula o n-esimo valor da sequencia de fibonacci.
+     * 
+     * @param n     numero do elemento da sequencia. Obrigatoriamente
+     *              deve ser maior que 0. (n > 0)
+     * 
+     * @return      valor do n-esimo elemento.
      */
-    public static int somaDoisTres(int[] vetor){
-        Arrays.sort(vetor);        
-        return vetor[vetor.length - 2] + vetor[vetor.length - 3];
-    }
-
-    //A
-    public static int somatorio(int[] vetor){
-        int resultado = 0;
-        for(int i = 0; i < vetor.length; i++){
-            resultado += vetor[i];
-        }
-        return resultado;
+    public static int fibonacci(int n) {
+        if(n < 2)return n;
+        else return fibonacci(n-1) + fibonacci(n-2);
     }
     
-    // 3-DONE
-    public static String getFirstAparicao(String s, char c)
-    {      
-        int i = s.indexOf(c)+1;
-        return s.substring(i);
-    }
-
-    public static String getLastAparicao(String s, char c)
-    {
-        int lastAparicao = s.lastIndexOf(c)+1;
-        String resp = s.substring(lastAparicao);
-        return resp;
-    }
-
-    public static String getBetweenAparicao(String s, char c)
-    {    
-        int firstAparicao = s.indexOf(c)+1;
-        int lastAparicao = s.lastIndexOf(c);
-
-        return s.substring(firstAparicao, lastAparicao);
-    }
-
-    // 4 - DONE
-    public static String findPal(String p)
-    { 
-        String ehPal = "Eh palindrome";
-        String naoEh = "Nao Eh Palindrome";
-        char word[] = p.toCharArray();
-        int aux1 = 0, aux2 = word.length-1;
-        while(aux2 > aux1)
-        {
-            if(word[aux1] == ' ')aux1++;
-            if(word[aux2] == ' ')aux2--;
-            if(word[aux1] != word[aux2])return naoEh;
-            ++aux1;
-            --aux2;
-        }
-        return ehPal;
-    }
-
-    public static String findPalEmptySpaces(String p)
-    {
-        return findPal(p);
-    }
-
-    // 5 DONE
-    public static void findPalInArr(String arr[])
-    {
-        int len = arr.length;
-        int i = 0;
-        String resp = "";
-        while(i < len)      
-        {
-            resp += arr[i]+" -> "+findPal(arr[i])+"\n";
-            i++;
-        }
-        System.out.println(resp);
+    /**
+     * Metodo somatorio.
+     * 
+     * @param 
+     * 
+     * @return
+     */
+    public static int somatorio(int[] vals) {
+        int ans = 0, i, len = vals.length;
+        for(i = 0;i < len; i +=1) ans += vals[i];
+        return ans;
     }
     
-    public static void findPalTenthousand() 
-    {
-        int tenThousand = 10001;
-        int i = 2;
-        String resp = "";
-        while(i < tenThousand)
-        {
-            String ans = findPal(String.valueOf(i));
-            int tamanhoDaRespostaDoPal = 17;
-            if(ans.length() < tamanhoDaRespostaDoPal) 
-            {
-                System.out.println(i+" -> "+findPal(String.valueOf(i)));
-            }
-            i += 2;
-        }
-    }    
-    
-    public static void somaUm(int[]a)
-    {
-        a =new int[]{1};
+    /**
+     * Metodo segundoTerceiro.
+     * 
+     * @param
+     * 
+     * @return
+     */
+    public static int segundoTerceiro(int[] vals) {
+        java.util.Arrays.sort(vals);
+        return vals[vals.length - 2] + vals[vals.length - 3];
     }
     
-    public static void print()
-    {
-        int [] valor = null;
-        somaUm(valor);
-        System.out.println(valor[0]);
+    /**
+     * Metodo dePrimeiroDelimAoFim.
+     * 
+     * @param
+     * 
+     * @return
+     */
+    public static String dePrimeiroDelimAoFim(String str, char delim) {
+        int firstdelim = str.indexOf(delim) + 1;
+        return str.substring(firstdelim);
+    }
+    
+    /**
+     * Metodo deUltimoDelimAoFim.
+     * 
+     * @param
+     * 
+     * @return
+     */
+    public static String deUltimoDelimAoFim(String str, char delim) {
+        int lastdelim = str.lastIndexOf(delim) + 1;
+        return str.substring(lastdelim);
+    }
+    
+    /**
+     * Metodo entreDelims.
+     * 
+     * @param
+     * 
+     * @return
+     */
+    public static String entreDelims(String str, char delim) {
+        int firstdelim  = str.indexOf(delim) + 1;
+        int lastdelim = str.lastIndexOf(delim);
         
+        if(lastdelim<0)return str;
+        if(lastdelim==0)lastdelim = str.length();
+        if(lastdelim < firstdelim) return new String("");
+        return str.substring(firstdelim, lastdelim) ;
     }
-    public static void main(String[]args)
-    {
-        
+    
+    /**
+     * Metodo isPalindromo.
+     * 
+     * @param
+     * 
+     * @return
+     */
+    public static boolean isPalindromo(String str) {
+        int n = str.length();
+        for(int i = 0; i < n/2; i++) {
+            if(str.codePointAt(i) != str.codePointAt(n-i-1))return false;
+        }
+        return true;
     }
+    
+    /**
+     * Metodo isPalindromoSemEspaco.
+     * 
+     * @param
+     * 
+     * @return
+     */
+    public static boolean isPalindromoSemEspaco(String str) {
+        str = str.replaceAll("\\s+", "");// "\\s+" -> espacos em brancos
+        return isPalindromo(str);
+    }
+    
+    
 }
