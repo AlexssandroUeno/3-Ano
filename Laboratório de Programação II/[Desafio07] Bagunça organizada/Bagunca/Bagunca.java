78d882f78f39e5ec381eb5214b4800a232f02f51
thelokys <ricardo.guidetti@hotmail.com>
2017-10-17 16:09:29 -0200

    Redes e matemática

--- Laboratório de Programação II/[Desafio07] Bagunça organizada/Bagunca/Bagunca.java
+++ Laboratório de Programação II/[Desafio07] Bagunça organizada/Bagunca/Bagunca.java
@@ -7,8 +7,40 @@
  */
 public class Bagunca
 {
+    
+    
     public static boolean isIntegerMinHeap(int[] arr) {
-        throw new RuntimeException("O aluno ainda não implementou essa funcionalidade");
+
+        
+        for(int i = 0; i<arr.length;i++){
+            int pai = i/2;
+            int child = 2*i+1; 
+            if(arr.length == 1)  return false;
+            if(pai > arr.length) return false;
+            
+            return mínimo(i);
+        }
+        return false;
+    }
+    
+    public static boolean mínimo(int i) {
+        int level = ((int)(Math.log(i)/Math.log(2))) + 1;
+        if(level % 2 == 0) {
+            return false;
+        }
+        return true;
+    }
+    
+    public static boolean isIntegerMaxHeap(int[] arr) {
+        return false;
+    }
+    
+    public static boolean isComparableMinHeap(int[] arr) {
+        return false;
+    }
+    
+    public static boolean isMinHeap(int[] arr) {
+        return false;
     }
     
     // Bagunça é uma classe utilitária, não deve ser instanciada
